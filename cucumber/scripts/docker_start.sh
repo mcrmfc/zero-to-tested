d1f2c0d642ce13e8cf46f30d64345bfd98cddb56
Matthew Robbins <matthew.robbins.ext@bbc.co.uk>
2016-05-06 17:11:11 +0100

    Add docker start stop scripts

---
+++ cucumber/scripts/docker_start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker/docker_run.sh zero/test $CONTAINER_NAME cucumber docker
+
+cd cucumber
+
+# ensure we have reports dir
+mkdir -p reports
+
+# bundle - note we are caching gems on the host file system to speed up docker start
+docker exec $CONTAINER_NAME bundle install --binstubs --path vendor/bundle
+
+# clean reports
+#docker exec $CONTAINER_NAME bin/rake clean
+
+# run tests
+docker exec $CONTAINER_NAME bin/cucumber
